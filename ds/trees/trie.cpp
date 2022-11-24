c492ec9adba1d4594c1d2065f96d940b3535fdb4
Amol <amolxp@gmail.com>
2022-11-24 21:58:29 +0530

    typed code for python-manim example, typing in progress for cpp-trie example

---
+++ ds/trees/trie.cpp
@@ -0,0 +1,33 @@
+using namespace std;
+
+const int ALPHABET_SIZE = 26;
+
+struct TrieNode {
+	struct TrieNode *children[ALPHABET_SIZE];
+	bool isEndOfWord;
+};
+
+struct TrieNode *getNode(void) {
+	struct TrieNode *pNode = new TrieNode();
+	pNode->isEndOfWord = false;
+	
+	for(int i = 0; i < ALPHABET_SIZE; i++) {
+		pNode->children[i] = NULL;
+	}
+
+	return pNode;
+}
+
+void insert(TrieNode *root, string key) {
+	struct TrieNode *pCrawl = root;
+
+	for(int i = 0; i < key.length(); i++) {
+		int index = key[i] - 'a';
+		if(pCrawl->children[index] == NULL) {
+			pCrawl->children[index] = getNode();
+		}
+		pCrawl = pCrawl->children[index];
+	}
+	pCrawl->isEndOfWord = true;
+}
+
